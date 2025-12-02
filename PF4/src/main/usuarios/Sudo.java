cb328f3faf94424f69fadcea5915112989355c4e
glugo-r <gabriel.lugo@ciencias.unam.mx>
2025-12-02 01:57:07 -0600

    Cantidad de Platillos

--- PF4/src/main/usuarios/Sudo.java
+++ PF4/src/main/usuarios/Sudo.java
@@ -3,10 +3,10 @@ package usuarios;
 public class Sudo extends Usuario {
     
     public Sudo() {
-        super("SuperAdmin", "sudo@restaurante.com", "Sudo");
+        super("SuperAdmin", "sudo@restaurante.com", "Sudo", "admin123"); // Contraseña por defecto
     }
     
-    public Administrador crearAdministrador(String nombre, String email) {
-        return new Administrador(nombre, email);
+    public Administrador crearAdministrador(String nombre, String email, String password) {
+        return new Administrador(nombre, email, password);
     }
 }
